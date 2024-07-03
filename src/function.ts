c19d7c1db245ffaeb0d0e778744d816837afbff1
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-03 13:52:42 +0530

    Functions

--- src/function.ts
+++ src/function.ts
@@ -1,7 +1,21 @@
 //Function
 
-type functionType = (n: number, m: number) => number;
+type functType = (n: number, m: number, l?: number) => number;
 
-const funci: functionType = (n, m) => {
-  return n * m;
+const funct: functType = (a, b, l) => {
+  if (typeof l === "undefined") return a*b;
+  return b * a;
 };
+funct(12, 13);
+
+
+//REST OPERATOR
+
+type operat = (p:number,q:number)=>number
+
+const operator = (...p:number[])=>{
+  
+  return p
+}
+
+operator(90,0,45,5,55,5,5,5,5,55,5,5,55,1,55,5,5,5.5,55,)
