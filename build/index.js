dc2c92f36e3e371bb3c5cb83e42a5ea14626f55f
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-03 14:18:30 +0530

    A

--- build/index.js
+++ build/index.js
@@ -1,7 +1,11 @@
 "use strict";
 // let surname:string | number;
 // surname = 90
-const func = (n, m) => {
-    console.log(m, n);
-    return m * n;
-};
+// const func =(n:number,m:number) : number =>{
+//     console.log(m,n)
+//     return m*n  
+// }
+let numberOne = 3;
+let numberTwo = "3";
+let sum = numberOne + numberTwo;
+console.log(sum);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 // let surname:string | number;
 // surname = 90
 
-const func =(n:number,m:number) : number =>{
-    console.log(m,n)
-    return m*n  
+// const func =(n:number,m:number) : number =>{
+//     console.log(m,n)
+//     return m*n  
 
-}
+// }
+
+let numberOne =3;
+let numberTwo = "3";
+let sum = numberOne+numberTwo;
+console.log(sum)
