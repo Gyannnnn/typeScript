69934be934ec36d8139d44eeefb67305d7676306
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-04 09:22:30 +0530

    functionInTypeScript

---
+++ build/finctionInTypescript.js
@@ -0,0 +1,6 @@
+"use strict";
+function addTwo(num) {
+    console.log(num);
+    return num + 23;
+}
+addTwo(99);
---
+++ src/finctionInTypescript.ts
@@ -0,0 +1,6 @@
+function addTwo(num: number){
+    console.log(num)
+    return num+23
+}
+
+addTwo(99)
