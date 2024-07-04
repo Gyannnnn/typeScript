a9fc6800ffe5e67edcae52294374483dbbd760fa
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-04 09:09:05 +0530

    numberAndBoolean

---
+++ build/number.js
@@ -0,0 +1,7 @@
+"use strict";
+let userId = 8984929824989243;
+let userId2 = 906281684912486.7813111331;
+let value = userId2.toFixed();
+console.log(value);
+// BOLLEAN
+let isLoggedIn = false;
---
+++ src/number.ts
@@ -0,0 +1,10 @@
+let userId: number = 8984929824989243
+
+let userId2: number =906281684912486.7813111331;
+let value = userId2.toFixed()
+console.log(value)
+
+
+// BOLLEAN
+
+let isLoggedIn: boolean = false
