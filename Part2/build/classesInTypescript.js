690f84bb126c7969f4d793185eb4df1839cc3366
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-15 20:09:15 +0530

    More About Classes

--- Part2/build/classesInTypescript.js
+++ Part2/build/classesInTypescript.js
@@ -6,11 +6,12 @@ class User {
         this.public = true;
         this.email = email;
         this.name = name;
+        this.constact;
     }
 }
 const Gyan = new User("gyanpatra@gamil.com", "Gyanranjan Patra");
 console.log(Gyan);
 Gyan.city = "Keonjhar";
-Gyan.constact = 7978648082;
+// Gyan.constact =7978648082
 Gyan.public = false;
 console.log(Gyan);
--- Part2/src/classesInTypescript.ts
+++ Part2/src/classesInTypescript.ts
@@ -2,17 +2,18 @@ class User{
     email:string
     name:string
     city:string = ""
-    constact:number = 0
+    private constact:number = 0
     public:boolean = true
     constructor(email:string,name:string){
         this.email = email;
         this.name = name
+        this.constact
     }
 }
 
 const Gyan = new User("gyanpatra@gamil.com","Gyanranjan Patra")
 console.log(Gyan)
 Gyan.city = "Keonjhar"
-Gyan.constact =7978648082
+// Gyan.constact =7978648082
 Gyan.public = false
 console.log(Gyan)
