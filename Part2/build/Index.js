552cfdd80aea51dc1911240a7553b1c846c3031f
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-15 19:30:05 +0530

    typeScript configuration

---
+++ Part2/build/Index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello World !");
+console.log("Typescript Is Owsome !");
+let a = 90;
+let b = 90;
+function sum(a, b) {
+    return a + b;
+}
---
+++ Part2/src/Index.ts
@@ -0,0 +1,8 @@
+console.log("Hello World !");
+console.log("Typescript Is Owsome !");
+
+let a:number =90;
+let b:number = 90;
+function sum(a:number,b:number): number{
+    return a+b
+}
